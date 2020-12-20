3dee8135fb650ec9aed7a75c7506ddfbc33c53fb
Aleksandr Fomin <fsasha_1997@mail.ru>
2020-12-20 15:29:40 +0300

    done]

--- lab5.c
+++ lab5.c
@@ -8,29 +8,38 @@
 
 #define NTHREADS 4
 
-#define pthread_parallel(arr1, arr2, size, func) {                                         \
-			int i, retval;                                                                 \
-			pthread_t threads[NTHREADS];                                                   \
-			struct thread_arg *args = malloc(sizeof(struct thread_arg) * NTHREADS);        \
-                                                                                           \
-			for (i = 0; i < NTHREADS; i++) {                                               \
-				args[i].arr = arr1;                                                        \
-				args[i].arr2 = arr2;                                                       \
-				args[i].size = size;                                                       \
-				args[i].thread_id = i;                                                     \
-				pthread_create(&threads[i], NULL, func, args + i);                         \
-			}                                                                              \
-                                                                                           \
-			for (i = 0; i < NTHREADS; i++)                                                 \
-				pthread_join(threads[i], &retval);                                         \
+#define pthread_parallel(arr1_param, arr_2_param, size_param, func_param) {					\
+			int i;																			\
+			void *retval;																	\
+			pthread_t threads[NTHREADS];													\
+			struct thread_arg *args = malloc(sizeof(struct thread_arg) * NTHREADS);			\
+																							\
+			for (i = 0; i < NTHREADS; i++) {												\
+				args[i].arr = arr1_param;													\
+				args[i].arr2 = arr_2_param;													\
+				args[i].size = size_param;													\
+				args[i].thread_id = i;														\
+				pthread_create(&threads[i], NULL, func_param, args + i);					\
+			}																				\
+																							\
+			for (i = 0; i < NTHREADS; i++)													\
+				pthread_join(threads[i], &retval);											\
 		}
 
 struct thread_arg {
 	double *arr;
 	double *arr2;
-	int	   thread_id;
+	int    thread_id;
 	int    size;
+	double x;
+	double y;
+	unsigned int *seedp;
+	int* status;
+	int chunk;
 };
+
+pthread_mutex_t status_lock;
+pthread_mutex_t shedule_dynamic_lock;
 
 struct timeval timeval;
 double omp_get_wtime()
@@ -39,17 +48,60 @@ double omp_get_wtime()
 	return (double)timeval.tv_sec + (double)timeval.tv_usec / 1000000;
 }
 
+void *fill_array_thread_func(void *arg)
+{
+	int i, j;
+	struct thread_arg *arg_str = arg;
+
+	int size = arg_str->size;
+	double left = arg_str->x;
+	double right = arg_str->y;
+	double *arr = arg_str->arr;
+	unsigned int *seedp = arg_str->seedp;
+	int *global_i = arg_str->status;
+	int chunk = arg_str->chunk;
+
+	while(1) {
+		pthread_mutex_lock(&shedule_dynamic_lock);
+		i = *global_i;
+		(*global_i) += chunk;
+		pthread_mutex_unlock(&shedule_dynamic_lock);
+
+		if (i >= size)
+			break;
+
+		for (j = i; j < (i + chunk) && j < size; j++, i++) {
+			unsigned int seed_i = i + *seedp;
+			arr[i] = rand_r(&seed_i) / (double)RAND_MAX * (right - left) + left;
+		}
+	}
+
+	pthread_exit(NULL);
+}
+
 void fill_array(double *arr, int size, double left, double right, unsigned int *seedp)
 {
 	int i;
+	int global_i = 0;
+	void *retval;
+	pthread_t threads[NTHREADS];
+	struct thread_arg *args = malloc(sizeof(struct thread_arg) * NTHREADS);
 
-	#if defined(_OPENMP)
-	#pragma omp parallel for default(none) private(i) shared(seedp, right, left, size, arr)
-	#endif
-	for (i = 0; i < size; i++) {
-		unsigned int seed_i = i + *seedp;
-		arr[i] = rand_r(&seed_i) / (double)RAND_MAX * (right - left) + left;
+	int chunk_size = 5;
+
+	for (i = 0; i < NTHREADS; i++) {
+		args[i].arr = arr;
+		args[i].x = left;
+		args[i].y = right;
+		args[i].seedp = seedp;
+		args[i].size = size;
+		args[i].status = &global_i;
+		args[i].chunk = chunk_size;
+		pthread_create(&threads[i], NULL, fill_array_thread_func, args + i);
 	}
+
+	for (i = 0; i < NTHREADS; i++)
+		pthread_join(threads[i], &retval);
 }
 
 void print_array(double *arr, int size)
@@ -64,6 +116,7 @@ void print_array(double *arr, int size)
 
 void *map_m1_thread_func(void *arg)
 {
+	int i;
 	struct thread_arg *arg_str = arg;
 
 	int thread_id = arg_str->thread_id;
@@ -73,6 +126,8 @@ void *map_m1_thread_func(void *arg)
 	for (i = thread_id; i < size; i += NTHREADS) {
 		arr[i] = tanh(arr[i]) - 1;
 	}
+
+	pthread_exit(NULL);
 }
 
 void map_m1(double *arr, int size)
@@ -82,6 +137,7 @@ void map_m1(double *arr, int size)
 
 void *map_m2_thread_func(void *arg)
 {
+	int i;
 	struct thread_arg *arg_str = arg;
 	
 	int thread_id = arg_str->thread_id;
@@ -95,6 +151,8 @@ void *map_m2_thread_func(void *arg)
 			prev = arr_copy[i - 1];
 		arr[i] = sqrt(exp(1.0) * (arr_copy[i] + prev));
 	}
+
+	pthread_exit(NULL);
 }
 
 void map_m2(double *arr, int size, double *arr_copy)
@@ -104,6 +162,7 @@ void map_m2(double *arr, int size, double *arr_copy)
 
 void *copy_arr_thread_func(void *arg)
 {
+	int i;
 	struct thread_arg *arg_str = arg;
 	
 	int thread_id = arg_str->thread_id;
@@ -114,6 +173,8 @@ void *copy_arr_thread_func(void *arg)
 	for (i = thread_id; i < size; i += NTHREADS) {
 		dst[i] = src[i];
 	}
+
+	pthread_exit(NULL);
 }
 
 void copy_arr(double *src, int len, double *dst)
@@ -123,6 +184,7 @@ void copy_arr(double *src, int len, double *dst)
 
 void *apply_merge_thread_func(void *arg)
 {
+	int i;
 	struct thread_arg *arg_str = arg;
 	
 	int thread_id = arg_str->thread_id;
@@ -133,11 +195,13 @@ void *apply_merge_thread_func(void *arg)
 	for (i = thread_id; i < size; i += NTHREADS) {
 		m2[i] = fabs(m1[i] - m2[i]);
 	}
+
+	pthread_exit(NULL);
 }
 
 void apply_merge_func(double *m1, double *m2, int m2_len)
 {
-	pthread_parallel(m1, m2, m2_len, copy_arr_thread_func);
+	pthread_parallel(m1, m2, m2_len, apply_merge_thread_func);
 }
 
 void heapify(double *array, int n)
@@ -233,36 +297,86 @@ double min_not_null(double *arr, int len)
 	return min_val;
 }
 
-double reduce(double *arr, int len)
+void *reduce_thread_func(void *arg)
 {
 	int i;
-	double min_val = min_not_null(arr, len);
+	struct thread_arg *arg_str = arg;
+	
+	int thread_id = arg_str->thread_id;
+	int size = arg_str->size;
+	double *arr = arg_str->arr;
+	double min_val = arg_str->x;
 	double x = 0;
 
-	#if defined(_OPENMP)
-	#pragma omp parallel for default(none) private(i) shared(arr, len, min_val) reduction(+:x)
-	#endif
-	for (i = 0; i < len; i++) {
+	for (i = thread_id; i < size; i += NTHREADS) {
 		if ((int)(arr[i] / min_val) % 2 == 0) {
 			double sin_val = sin(arr[i]);
 			x += sin_val;
 		}
 	}
+
+	arg_str->x = x;
+
+	pthread_exit(NULL);
+}
+
+double reduce(double *arr, int len)
+{
+	int i;
+	void *retval;
+	double x = 0;
+	pthread_t threads[NTHREADS];
+	struct thread_arg *args = malloc(sizeof(struct thread_arg) * NTHREADS);
+
+	double min_val = min_not_null(arr, len);
+
+	for (i = 0; i < NTHREADS; i++) {
+		args[i].arr = arr;
+		args[i].x = min_val;
+		args[i].size = len;
+		args[i].thread_id = i;
+		pthread_create(&threads[i], NULL, reduce_thread_func, args + i);
+	}
+
+	for (i = 0; i < NTHREADS; i++) {
+		pthread_join(threads[i], &retval);
+		x += args[i].x;
+	}
+
 	return x;
 }
 
-void do_main(int argc, char* argv[], int *status)
+void *sort_thread_func(void *arg)
 {
-	int i, N, N2;
+	struct thread_arg *arg_str = arg;
+	
+	int size = arg_str->size;
+	double *arr = arg_str->arr;
+
+	heapsort(arr, size);
+
+	pthread_exit(NULL);
+}
+
+void *do_main(void *arg)
+{
+	int i, N2;
+	void *retval;
 	double T1, T2;
 	long delta_ms;
 	double *M1, *M2, *M2_copy, *MERGED;
 	int A = 540;
 	unsigned int seed1, seed2;
 	// double X;
-	int iter = 10;
+	int iter = 1;
+	pthread_t threads[2];
+	struct thread_arg *args = malloc(sizeof(struct thread_arg) * 2);
 
-	N = atoi(argv[1]); /* N равен первому параметру командной строки */
+	struct thread_arg *arg_str = arg;
+	
+	int N = arg_str->y;
+	int *status = arg_str->status;
+
 	T1 = omp_get_wtime(); /* запомнить текущее время T1 */
 
 	M1 = malloc(sizeof(double) * N);
@@ -272,12 +386,9 @@ void do_main(int argc, char* argv[], int *status)
 
 	for (i = 0; i < iter; i++) /* 50 экспериментов */
 	{	
-		#if defined(_OPENMP)
-		#pragma omp critical
-		{
-			*status = 100 * (i + 1) / iter;
-		}
-		#endif
+		pthread_mutex_lock(&status_lock);
+		*status = 100 * (i + 1) / iter;
+		pthread_mutex_unlock(&status_lock);
 
 		seed1 = i;
 		seed2 = i;
@@ -292,18 +403,20 @@ void do_main(int argc, char* argv[], int *status)
 
 		N2 = N / 2;
 
-		#if defined(_OPENMP)
-		#pragma omp parallel sections
-		{
-				#pragma omp section
-				heapsort(M2, N2 / 2);
-				#pragma omp section
-				heapsort(M2 + (N2 / 2), (N2 + 1) / 2);
-		}
+		args[0].arr = M2;
+		args[0].size = N2 / 2;
+		pthread_create(&threads[0], NULL, sort_thread_func, args);
+
+		args[1].arr = M2 + (N2 / 2);
+		args[1].size = (N2 + 1) / 2;
+		pthread_create(&threads[1], NULL, sort_thread_func, args + 1);
+
+		for (i = 0; i < 2; i++)
+			pthread_join(threads[i], &retval);
+
 		mergeArrays(MERGED, M2, M2 + (N2 / 2), N2 / 2, (N2 + 1) / 2);
-		#else
-		heapsort(M2, N2);
-		#endif
+
+		// print_array(MERGED, N / 2);
 
 		reduce(MERGED, N / 2);
 		// printf("X = %f\n", X);
@@ -317,44 +430,46 @@ void do_main(int argc, char* argv[], int *status)
 	free(M2);
 	free(M2_copy);
 	free(MERGED);
+
+	pthread_exit(NULL);
 }
 
-#if defined(_OPENMP)
-void do_timer(int *status)
+void *do_timer(void *status)
 {
 	int val;
 
-	#pragma omp critical
-	{
-		val = *status;
-	}
+	pthread_mutex_lock(&status_lock);
+	val = *((int *) status);
+	pthread_mutex_unlock(&status_lock);
 
 	while(val < 100) {
-		#pragma omp critical
-		{
-			val = *status;
-		}
+		pthread_mutex_lock(&status_lock);
+		val = *((int *) status);
+		pthread_mutex_unlock(&status_lock);
 		printf("Status = %d%%\n", val);
 		sleep(1);
 	}
+
+	pthread_exit(NULL);
 }
-#endif
 
 int main(int argc, char* argv[])
 {
 	int status = 0;
+	int i;
+	void *retval;
+	pthread_t threads[2];
+	struct thread_arg *args = malloc(sizeof(struct thread_arg));
+	int N = atoi(argv[1]);
 
-	#if defined(_OPENMP)
-	#pragma omp parallel sections shared(status)
-	{
-		#pragma omp section
-		do_timer(&status);
-		#pragma omp section
-		do_main(argc, argv, &status);
-	}
-	#else
-		do_main(argc, argv, &status);
-	#endif
+	pthread_create(&threads[0], NULL, do_timer, &status);
+
+	args->y = N;
+	args->status = &status;
+	pthread_create(&threads[1], NULL, do_main, args);
+
+	for (i = 0; i < 2; i++)
+		pthread_join(threads[i], &retval);
 
 	return 0;
 }
